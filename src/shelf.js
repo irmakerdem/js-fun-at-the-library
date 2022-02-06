b3ea7c21bd27a4dd1a652b2a4f29733f7dbe6255
Irmak Erdem <irmakerdem9@gmail.com>
2022-02-05 19:50:22 -0700

    Add answers

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,63 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+};
+
+function unshelfBook(bookTitleString, shelfArray) {
+  for (var i = 0; i < shelfArray.length; i++) {
+    if (shelfArray[i].title === bookTitleString) {
+      shelfArray.splice(i, 1);
+    }
+  }
+}
+
+// console.log(shelf[0].title);
+// console.log(shelf[1].title);
+// console.log(shelf[2].title);
+
+
+
+//access title
+//access all titles (for loop)
+//compare each key title with bookTitle
+//if (2 strings match){remove book object from shelf array}
+
+
+
+function listTitles(shelfArray) {
+  var titles = [];
+  for (var i = 0; i < shelfArray.length; i++) {
+     titles.push(shelfArray[i].title);
+  }
+  return titles.join(', ');
+}
+
+
+
+
+
+
+function searchShelf(shelfArray, titleString) {
+for (var i = 0; i < shelfArray.length; i++) {
+  if (shelfArray[i].title === titleString) {
+      return true;
+  }
+  }
+  return false
+}
+var shelfArray = [];
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
+
+
+//from shelf.js using module.exports,
+//moves shelf.js to computer galaxy
+//shelf-test imports from source shelf.js
