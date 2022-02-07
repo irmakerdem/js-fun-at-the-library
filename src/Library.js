c57a3f8c811bce7b3962eebc22a2203213fc4908
Irmak Erdem <irmakerdem9@gmail.com>
2022-02-06 17:30:52 -0700

    Add answers

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,152 @@
+// function createLibrary(name) {
+//   var library = {
+//     name: name,
+//     shelves: { [genre]
+//   }
+//   };
+//   return library
+// }
 
+
+
+// function createLibrary(name) {
+//   var library = {
+//     name: name,
+//     shelves: { genre: {}
+//   }
+//   };
+//   return library
+// }
+
+// library.shelves.genre = [];
+// var genreArray = library.shelves.genre;
+// var genreArray = [library.shelves.genre];
+
+
+
+// function createLibrary(name) {
+//   var library = {
+//     name: name,
+//     shelves: { genre: {[]}
+//   }
+//   };
+//   return library
+// }
+
+
+
+// function createLibrary(name) {
+//   var library = {
+//     name: name,
+//     shelves: {
+//       fantasy: [],
+//       fiction: [],
+//       nonFiction: [],
+//   }
+//   };
+//   return library
+// }
+
+
+function createLibrary(name) {
+    return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+  }
+  };
+}
+
+//THIS ALSO WORKS:
+// function createLibrary(name) {
+//   var library = {};
+// library.name = name
+// console.log(library);
+// library.shelves = {};
+// console.log(library);
+// library.shelves.fantasy = [];
+// console.log(library);
+// library.shelves.fiction = [];
+// console.log(library);
+// library.shelves.nonFiction = [];
+// console.log(library);
+// return library;
+
+
+
+
+
+// function addBook(library, book) {
+//   library.shelves.fantasy.push(book)
+// }
+
+function addBook(library, book) {
+  if (book.genre === "fantasy") {
+    library.shelves.fantasy.unshift(book)
+  } else if (book.genre === "nonFiction") {
+    library.shelves.nonFiction.unshift(book)
+  } else {
+    library.shelves.fiction.unshift(book)
+  }
+};
+
+
+//THIS ALSO WORKS:
+// function addBook(library, book) {
+//   library.shelves[book.genre].unshift(book)
+// };
+
+
+
+
+//functioncheckoutBook(object, string, string)
+function checkoutBook(library, title, genre) {
+  if (genre === library.shelves[genre]) {
+    library.shelves[genre].splice(0, 1);
+};
+return `You have now checked out ${title} from the ${library}`
+}
+
+
+  // console.log(library.shelves.genre)
+  // //undefined
+  //
+  // console.log(genre);
+  // //fiction
+  //
+  // console.log(library.shelves[genre]);
+  // //prideAndPrejudice
+  //
+  // console.log(library.shelves.fiction);
+  // //prideAndPrejudice
+  //
+  // console.log(library.shelves["fiction"]);
+  // //prideAndPrejudice
+  //
+  // console.log(library.shelves["genre"]);
+  // //undefined
+  // //does "genre" instead of genre parameter
+
+
+//goal:checkout a book
+//access an array of book objects
+//shelves is an object with 3 keys which are arrays of objects
+
+// ['string'] = dot notation
+//
+
+
+
+
+// library.shelves.book.genre
+// library.shelves[book.genre]
+// library.shelves[genre]
+// library.shelves["fiction"]
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook,
 };
